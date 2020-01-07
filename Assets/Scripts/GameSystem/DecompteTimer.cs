b57ed5d3e90c73189d1b0c5086d62e33a071e43e
Sofiane <sofiane.benembareck@hotmail.com>
2020-01-07 09:19:09 +0100

    Add game & decompte timer + float event

---
+++ Assets/Scripts/GameSystem/DecompteTimer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DecompteTimer : MonoBehaviour
+{
+    [Header("Parameter")] 
+    [SerializeField] private float decompteTime = 3.0f;
+    private float currentDecompteTimer = 0.0f;
+    
+    [Header("Events")] 
+    [SerializeField] private FloatEvent OnSecondElapsed_DecompteTimer;
+    [SerializeField] private UnityEvent OnTimeElapsed_DecompteTimer;
+    
+    // ------------------
+    private Coroutine timer;
+    private WaitForSeconds oneSecond;
+    
+    private void Awake()
+    {
+        this.oneSecond = new WaitForSeconds(1.0f);
+    }
+    
+    #region Timer Functions
+
+    public void StartDecompteTimer()
+    {
+        if(timer == null)
+            timer = StartCoroutine(nameof(DecompteTimerClock));
+    }
+
+    public void StopDecompteTimer()
+    {
+        StopCoroutine(timer);
+        timer = null;
+    }
+
+    #endregion
+    
+    #region Utils Functions
+    
+    public void SetDecompteTimer(float time)
+    {
+        this.currentDecompteTimer = time;
+    }
+    
+    public void AddTimeToDecompteTimer(float time)
+    {
+        this.currentDecompteTimer += time;
+    }
+
+    public void RemoveTimeToDecompteTimer(float time)
+    {
+        this.currentDecompteTimer -= time;
+    }
+
+    public void ResetDecompteTimer()
+    {
+        this.currentDecompteTimer = this.decompteTime;
+    }
+    
+    #endregion
+    
+    private IEnumerator DecompteTimerClock()
+    {
+        while (this.currentDecompteTimer > 0.0f)
+        {
+            yield return oneSecond;
+            this.currentDecompteTimer -= 1.0f;
+            this.OnSecondElapsed_DecompteTimer.Invoke(this.currentDecompteTimer);
+        }
+        
+        this.OnTimeElapsed_DecompteTimer.Invoke();
+        
+        yield break;
+    }
+}
---
+++ Assets/Scripts/GameSystem/GameTimer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameTimer : MonoBehaviour
+{
+    [Header("Parameter")] 
+    [SerializeField] private float gameTime = 120.0f;
+    [SerializeField] private float[] onSpecifiedSecondElapsed_Value;
+    private float currentGameTimer = 0.0f;
+    
+    [Header("Events")] 
+    [SerializeField] private FloatEvent OnSecondElapsed_GameTimer;
+    [Space(5)]
+    [SerializeField] private UnityEvent[] OnSpecifiedSecondElapsed;
+    [Space(10)]
+    [SerializeField] private UnityEvent OnTimeElapsed_GameTimer;
+    
+    // ------------------
+    private Coroutine timer;
+    private WaitForSeconds oneSecond;
+    private int currentIndexSpecifiedSeconds = 0;
+    
+    private void Awake()
+    {
+        this.oneSecond = new WaitForSeconds(1.0f);
+    }
+
+    #region Timer Functions
+
+    public void StartGameTimer()
+    {
+        if(timer == null)
+            timer = StartCoroutine(nameof(GameTimerClock));
+    }
+
+    public void StopGameTimer()
+    {
+        StopCoroutine(timer);
+        timer = null;
+    }
+
+    public void StopAndResetGameTimer()
+    {
+        StartGameTimer();
+        ResetGameTimer();
+        OnSecondElapsed_GameTimer.Invoke(this.currentGameTimer);
+    }
+
+    #endregion
+
+    #region Utils Functions
+
+    public void SetGameTimer(float time)
+    {
+        this.currentGameTimer = time;
+    }
+    
+    public void AddTimeToGameTimer(float time)
+    {
+        this.currentGameTimer += time;
+    }
+
+    public void RemoveTimeToGameTimer(float time)
+    {
+        this.currentGameTimer -= time;
+    }
+
+    public void ResetGameTimer()
+    {
+        this.currentGameTimer = gameTime;
+    }
+    
+    #endregion
+
+    private IEnumerator GameTimerClock()
+    {
+        while (this.currentGameTimer > 0.0f)
+        {
+            yield return oneSecond;
+            this.currentGameTimer -= 1.0f;
+            this.OnSecondElapsed_GameTimer.Invoke(this.currentGameTimer);
+
+            if (this.currentIndexSpecifiedSeconds < this.onSpecifiedSecondElapsed_Value.Length)
+            {
+                if (this.onSpecifiedSecondElapsed_Value[this.currentIndexSpecifiedSeconds] <= this.currentGameTimer)
+                {
+                    this.OnSpecifiedSecondElapsed[this.currentIndexSpecifiedSeconds].Invoke();
+                    this.currentIndexSpecifiedSeconds++;
+                }
+            }
+            
+        }
+        
+        this.OnTimeElapsed_GameTimer.Invoke();
+        
+        yield break;
+    }
+}
